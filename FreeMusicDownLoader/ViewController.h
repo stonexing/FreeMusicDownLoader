a12c506fa0da0f950b567437b2e594dd42ffd3d7
Xingyan <xingyan@XingyandeMac-Pro.local>
2014-03-11 11:27:48 +0800

    Initial Commit

---
+++ FreeMusicDownLoader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FreeMusicDownLoader
+//
+//  Created by Xingyan on 14-3-11.
+//  Copyright (c) 2014年 Xingyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ FreeMusicDownLoader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FreeMusicDownLoader
+//
+//  Created by Xingyan on 14-3-11.
+//  Copyright (c) 2014年 Xingyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
